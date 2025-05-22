a4a90002e22ef220796da4c6286075034ccf577d
MrSociety404 <fabrice@altab.be>
2025-05-22 13:23:10 +0200

    chore: remove unused configuration files and dependencies; delete .editorconfig, .prettierignore, .prettierrc.json, and eslint.config.mjs

--- .antelope/interfaces.d/database-decorators/beta/index.d.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata';
-export * from './table';
-export * from './model';
-export * from './database';
-export * from './common';
-export * from './modifiers';
-export * from './schema';
